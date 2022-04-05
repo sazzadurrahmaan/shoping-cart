2d0fe28e1bdb5a88b4d6511e9828e55a11489ef7
Sazzad12340 <sazzadrn17@gmail.com>
2022-04-05 16:59:41 +0600

    shoping cart

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,66 @@
 const addButton12 = document.getElementById("addButton12");
 addButton12.addEventListener("click", () => {
-  const insert12 = document.getElementById("insert12");
-  const insertAmount12 = parseFloat(insert12.value);
-
-  const iphone12Price = document.getElementById("iphone12Price");
-  const current12Price = parseFloat(iphone12Price.innerText);
-
-  const total12insert = insertAmount12 + 1;
-
-  document.getElementById("insert12").value = total12insert;
-
-  let total12price = total12insert * 1219;
-
-  document.getElementById("iphone12Price").innerText = total12price;
+  handlephone(true);
 });
 
 const minus12 = document.getElementById("minus12");
 minus12.addEventListener("click", () => {
-  const insert12 = document.getElementById("insert12");
-  const insertAmount12 = parseFloat(insert12.value);
-
-  const iphone12Price = document.getElementById("iphone12Price");
-  const current12Price = parseFloat(iphone12Price.innerText);
-
-  const total12insertMinus = insertAmount12 - 1;
-
-  document.getElementById("insert12").value = total12insertMinus;
-
-  const total12priceMinus = current12Price - 1219;
-
-  document.getElementById("iphone12Price").innerText = total12priceMinus;
+  handlephone(false);
 });
 
-// iphone 11 side
+function handlephone(isIncrease) {
+  const insert12 = document.getElementById("insert12");
+  const insertAmount12 = parseInt(insert12.value);
 
+  const iphone12Price = document.getElementById("iphone12Price");
+  const current12Price = parseInt(iphone12Price.innerText);
+  let total12insert = insertAmount12;
+  if (isIncrease == true) {
+    total12insert = insertAmount12 + 1;
+  }
+  if (isIncrease == false && insertAmount12 > 0) {
+    total12insert = insertAmount12 - 1;
+  }
+
+  document.getElementById("insert12").value = total12insert;
+
+  const total12Price = total12insert * 1219;
+
+  document.getElementById("iphone12Price").innerText = total12Price;
+}
+
+//case section
 const addButton11 = document.getElementById("addButton11");
-addButton11.addEventListener("click", () => {
-  const insert11 = document.getElementById("insert11");
-  const insertAmount11 = parseFloat(insert11.value);
-
-  const iphone11Price = document.getElementById("iphone11Price");
-  const current11Price = parseFloat(iphone11Price.innerText);
-
-  const total11insert = insertAmount11 + 1;
-
-  document.getElementById("insert11").value = total11insert;
-
-  let total11price = total11insert * 59;
-
-  document.getElementById("iphone11Price").innerText = total11price;
+addButton11.addEventListener("click", function () {
+  handleProductChange(true);
 });
 
 const minus11 = document.getElementById("minus11");
 minus11.addEventListener("click", () => {
-  const insert11 = document.getElementById("insert11");
-  const insertAmount11 = parseFloat(insert11.value);
-
-  const iphone11Price = document.getElementById("iphone11Price");
-  const current11Price = parseFloat(iphone11Price.innerText);
-
-  const total11insertMinus = insertAmount11 - 1;
-
-  document.getElementById("insert11").value = total11insertMinus;
-
-  const total11priceMinus = current11Price - 59;
-
-  document.getElementById("iphone11Price").innerText = total11priceMinus;
+  handleProductChange(false);
 });
 
-// remove the element
+function handleProductChange(isIncrease) {
+  const insert11 = document.getElementById("insert11");
+  const insertAmount11 = parseInt(insert11.value);
+
+  const iphone11Price = document.getElementById("iphone11Price");
+  const current11Price = parseInt(iphone11Price.innerText);
+
+  let total11insert = insertAmount11;
+  if (isIncrease == true) {
+    total11insert = insertAmount11 + 1;
+  }
+  if (isIncrease == false && insertAmount11 > 0) {
+    total11insert = insertAmount11 - 1;
+  }
+
+  document.getElementById("insert11").value = total11insert;
+
+  const total11priceMinus = total11insert * 59;
+
+  document.getElementById("iphone11Price").innerText = total11priceMinus;
+}
 
 const removeItem = document.getElementById("remove-item");
 const removeItem2 = document.getElementById("remove-item2");
@@ -85,10 +77,10 @@ const checkOut = document.getElementById("checkout");
 
 checkOut.addEventListener("click", () => {
   const iphone12Price = document.getElementById("iphone12Price");
-  const current12Price = parseFloat(iphone12Price.innerText);
+  const current12Price = parseInt(iphone12Price.innerText);
 
   const iphone11Price = document.getElementById("iphone11Price");
-  const current11Price = parseFloat(iphone11Price.innerText);
+  const current11Price = parseInt(iphone11Price.innerText);
 
   const totalprice = current12Price + current11Price;
 
